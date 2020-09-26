2702a6eca7be265a54070edc0c84bd97b61a5748
songhe <songhe156@sina.com>
2020-09-26 17:52:07 +0800

    修复跳转链接

--- larabbs_one/resources/views/layouts/_header.blade.php
+++ larabbs_one/resources/views/layouts/_header.blade.php
@@ -17,8 +17,28 @@
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
+        @guest
         <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">登录</a></li>
         <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">注册</a></li>
+        @else
+        <li class="nav-item dropdown">
+          <a href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">
+            <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px" alt="头像" title="头像">
+            {{ Auth::user()->name }}
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a href="" class="dropdown-item">个人中心</a>
+            <a href="" class="dropdown-item">编辑资料</a>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item" id="logout">
+              <form action="{{ route('logout') }}" method="POST">
+                {{ csrf_field() }}
+                <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+              </form>
+            </a>
+          </div>
+        </li>
+        @endguest
       </ul>
     </div>
   </div>
