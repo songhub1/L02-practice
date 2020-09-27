51364fc42465f89f1e0c07978ab3092073fdb7b9
songhe <songhe156@sina.com>
2020-09-27 13:56:21 +0800

    显示头像

--- larabbs_one/resources/views/layouts/_header.blade.php
+++ larabbs_one/resources/views/layouts/_header.blade.php
@@ -23,7 +23,7 @@
         @else
         <li class="nav-item dropdown">
           <a href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">
-            <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px" alt="头像" title="头像">
+            <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px" alt="头像" title="头像">
             {{ Auth::user()->name }}
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
